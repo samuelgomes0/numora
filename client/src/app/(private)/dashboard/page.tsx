b43a152e2538e48ac267cb171699a275646b0c9b
Samuel Gomes <samuel_gomes26@hotmail.com>
2025-05-12 23:53:06 -0300

    ♻️ (dashboard/page.tsx): refatora a página do dashboard para exibir uma mensagem de boas-vindas ao usuário em vez de resumos financeiros
    🗑️ (Wise Wallet): remove arquivos e pastas não utilizados do projeto para
    limpar a estrutura do código
    
    🔧 (tsconfig.json): altera o diretório raiz do TypeScript para facilitar
    a organização do projeto

--- client/src/app/(private)/dashboard/page.tsx
+++ client/src/app/(private)/dashboard/page.tsx
@@ -1,38 +1,15 @@
-import {
-  BanknoteArrowDownIcon,
-  BanknoteArrowUpIcon,
-  BanknoteIcon,
-} from "lucide-react";
-import SummaryCard from "./components/SummaryCard";
-
-function DashboardPage() {
-  const finances = {
-    entries: 1500.0,
-    exits: 500.0,
+function Dashboard() {
+  const user = {
+    id: "1",
+    name: "John Doe",
   };
 
   return (
-    <div className="flex flex-col gap-4">
+    <div>
       <h1>Dashboard</h1>
-      <div className="flex gap-4 w-full">
-        <SummaryCard
-          icon={BanknoteArrowUpIcon}
-          title="Entradas"
-          content={finances.entries}
-        />
-        <SummaryCard
-          icon={BanknoteArrowDownIcon}
-          title="Saídas"
-          content={finances.exits}
-        />
-        <SummaryCard
-          icon={BanknoteIcon}
-          title="Balanço"
-          content={finances.entries - finances.exits}
-        />
-      </div>
+      <h2 className="">Hi, {user.name}! Welcome back.</h2>
     </div>
   );
 }
 
-export default DashboardPage;
+export default Dashboard;
